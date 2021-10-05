d552c3fbb13983b2fe0b012cc9f295345ebf63ea
bonot9434 <bonot9434@gmail.com>
2021-10-05 14:59:53 +0000

    [add]コントローラー

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,22 @@
+class TweetsController < ApplicationController
+  
+  def show
+  end
+  
+  def index
+    @tweets = Tweet.all
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image, :give, :take, :industry, :is_deleted)
+  end
+
 end
