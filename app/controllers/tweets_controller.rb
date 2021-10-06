162aaf9dbb5b18bab855c499e63548c9998a8ad5
bonot9434 <bonot9434@gmail.com>
2021-10-06 03:18:21 +0000

    [add]画像投稿

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,31 @@
 class TweetsController < ApplicationController
-  
+
   def show
   end
-  
+
   def index
     @tweets = Tweet.all
+    @tweet = Tweet.new
   end
-  
+
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  def tweet_params
+    params.require(:tweet).permit(:body, :image)
+  end
+
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ApplicationRecord
+  
+  belongs_to :user
+  attachment :image
+  
 end
