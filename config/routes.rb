47ac08b4706b260997983ea687e364cd8370d8dc
bonot9434 <bonot9434@gmail.com>
2021-10-09 17:59:07 +0000

    フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :users do
-    member do
-      get :likes
-    end
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
