b89b502bdea486d23779f591c93d83f666d431e7
bonot9434 <bonot9434@gmail.com>
2021-10-14 06:15:24 +0000

    マイページ編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,17 +4,12 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     like = @post.likes.new(user_id: current_user.id)
     like.save
-    redirect_to request.referer
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     like = @post.likes.find_by(user_id: current_user.id)
     like.destroy
-<<<<<<< Updated upstream
-    redirect_to request.referer
-=======
->>>>>>> Stashed changes
   end
   
 end
