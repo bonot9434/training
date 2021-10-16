7e79c30249fc0db24aab3268aadf0d503e4b3db0
bonot9434 <bonot9434@gmail.com>
2021-10-16 16:48:30 +0000

    検索機能追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,17 @@
 class SearchesController < ApplicationController
 
+  def search
+    @model=params[:model]
+    @content=params[:content]
+    if @model == 'user'
+      @records = User.where('name LIKE ?', '%'+@content+'%')
+    elsif @model == 'give'
+      @records = User.where('give LIKE ?', '%'+@content+'%')
+    else @model == 'take'
+      @records = User.where('take LIKE ?', '%'+@content+'%')
+    end
+  end
+
   def search_tag
     @tag_list = Tag.all.order(name: "ASC")
     @tag = Tag.find(params[:tag_id])
