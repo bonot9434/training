d8700238c021d29a4c7b4471ee6143bf539daf01
bonot9434 <bonot9434@gmail.com>
2021-10-16 05:41:25 +0000

    通知機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   # relationships(中間テーブル)を通してfollowedを参照→followings、一覧ページ作成のため
   has_many :followings, through: :relationships, source: :followed
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
