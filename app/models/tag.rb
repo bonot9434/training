9ae9cb8d1a2734c961fb6cbd2bb79df2f7c676c7
bonot9434 <bonot9434@gmail.com>
2021-10-27 02:19:26 +0000

    レイアウト編集

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,6 @@ class Tag < ApplicationRecord
   has_many :post_tags,dependent: :destroy, foreign_key: 'tag_id'
   # post_tagsを通して参照できる
   has_many :posts,through: :post_tags
-  # validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true
 
 end
